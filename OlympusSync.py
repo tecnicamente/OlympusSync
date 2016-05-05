b1f043cc638f203dbe12984ca6fa8f7357b08ed8
Tecnicamente <lippoassoluto@yahoo.it>
2016-05-05 22:37:54 +0200

    OlympusSync - Download images via WiFi from your Olympus OMD E-M10
    The tool is stil in development, it will allow you to download images
    from your great E-M10 camera, and probably also on E-M5 and E-M1.

--- OlympusSync.py
+++ OlympusSync.py
@@ -1,30 +1,39 @@
-""" NOT WORKING """
+# -*- coding: utf-8 -*-
+
+# Created on  05/05/2015
+# @author: Tecnicamente
+# License GPL3
+# Version 1.0Beta
+
 import urllib
 import urllib2
-
-""" CONFIGURATION PARAMETERS """
-
-UrlToOpen = "http://oishare/DCIM/100OLYMP"
-
-""" END						 """
 import string
 import sys
-import urllib2
+
+# TODO LIST
+# =========
+# 1. PARAMETERS CHECK
+# 2. HELP MANIUAL
+# 3. CONTROL FOR EXISTING ALREADY DOWNLOADE FILES
+# 4. LOG FILE
+# 5. EXTERNAL SETTING FILE
+# 6. ROBUST ERRORS CHECK
+
+# CONFIGURATION PARAMETERS
+UrlToOpen = "http://oishare/DCIM/100OLYMP"
+# END
+
 
 try:
     OlympusHost = urllib2.urlopen(UrlToOpen)
     print ">>	" +UrlToOpen + " open"
-    """ DEBUG """
 
 except:
     print ">>	An error occured"
 
-"""
-except URLError, e:
-    print "An error occured"
-    print e.reason
-"""
-
+#except URLError, e:
+#    print "An error occured"
+#    print e.reason
 
 imagesName = []
 rowNumber = 0
@@ -41,17 +50,20 @@ while row:
 	rowNumber = rowNumber + 1
 	row = OlympusHost.readline()
 
-print imagesName # DEBUG
+#print imagesName # DEBUG
 
 print ">>	Found " + str(numberOfImages) + " images on the device"
 
+page = urllib2.urlopen('http://192.168.0.10/DCIM').read()
 for i in range(numberOfImages):
-	image = urllib2.urlopen("http://oishare/DCIM/100OLYMP/FILE.html?/DCIM/100OLYMP/"+(imagesName[i]))
+#	image = urllib2.urlopen("http://oishare/DCIM/100OLYMP/FILE.html?/DCIM/100OLYMP/"+(imagesName[i]))
 #	outputFile = open("/tmp/"+imagesName[i], "wb")
 #	outputFile.write(image.read())
 #	outputFile.close()
-	source = "http://oishare/DCIM/100OLYMP/FILE.html?/DCIM/100OLYMP/" + imagesName[i]
+
+	source = "http://oishare/DCIM/100OLYMP/" + imagesName[i]
 	destination = "/tmp/" + imagesName[i]
+	print ">>	" + source + " ---> " + destination
 	urllib.urlretrieve(source ,destination)
 	print ">> Image " + imagesName[i] + " downloaded"
 
